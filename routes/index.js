9f90100efa98f43e126e325acb70adcdbd8a23b2
Mitch Hunter <144734462+utahphotohunter@users.noreply.github.com>
2025-09-21 23:49:09 -0600

    finished 1st assignment

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+// Imports
+const routes = require('express').Router();
+const contacts = require('../controllers/contacts')
+
+
+
+// Define a route
+routes.get('/', contacts.estherRoute);
+
+routes.get('/michael', contacts.michaelRoute);
+
+routes.get('/alana', contacts.alanaRoute);
+
+module.exports = routes;
